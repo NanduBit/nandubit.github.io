aa04be01a88e567e239bb34c1ad726c06f5ecfcb
Nandu <89532571+NanduBit@users.noreply.github.com>
2026-08-06 01:53:30 +0530

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -77,7 +77,7 @@ window.siteConfig = {
       name: 'Discord-Nuke-Bot',
       title: 'Discord-Nuke-Bot',
       url: 'https://github.com/nandubit/Discord-Nuke-Bot',
-      description: 'A discord bot for server nuking and moderation with powerful tools.',
+      description: 'A discord bot for server nuking with powerful tools.',
       icon: `
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
           stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
